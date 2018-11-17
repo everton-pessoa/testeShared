838636956bc507c527ad8e4da2ba20341db8f3b7
Everton Pessoa <evertonalvespessoa@gmail.com>
2018-11-17 19:35:33 -0200

    testeShared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ItemComponent } from './item/item.component';
 
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
